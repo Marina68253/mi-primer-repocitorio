6f13c51fdd73241836622d778602b9f4f69f9af0
Marina68253 <mari68253@gmail.com>
2024-05-08 20:07:39 -0300

    guiones.ts

---
+++ guiones.ts
@@ -0,0 +1,10 @@
+import * as  rls from 'readline-sync';
+
+function dibujarGuiones(numeroGuiones : number){
+    let i: number;
+    let linea: string="";
+    for (i= 0; i <= numeroGuiones; i++){
+        linea=linea + "-";
+    }
+    console.log("linea");
+}
